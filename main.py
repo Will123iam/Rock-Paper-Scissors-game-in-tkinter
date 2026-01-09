610ab46809c3039b29067977ea86a6d932e66d2c
Will123iam <98259777+Will123iam@users.noreply.github.com>
2026-01-09 15:29:03 +0000

    runs the command for loading scores into the table

--- main.py
+++ main.py
@@ -3,17 +3,26 @@ from rock_paper_sisors import *
 import tkinter as tk
 import time
 
-
+#Tab 1 functions
 def start_game():
     num_rounds()
 
-
-
-#Tab 1 functions
 welcome()
 
 rock_img_butt = tk.Button(game_tab, command=start_game, image=rock_img)
 rock_img_butt.pack()
+
+#Tab 2 functions
+def get_scores():
+    names=[['9/1/2026','4','3','Player'], #Temp score stroage
+           ['9/1/2026','4','3','Player'],
+           ['9/1/2026','4','3','Player'],
+           ['9/1/2026','4','3','Player'],]
+
+    return names
+
+display_scores_table(load_scores())
+
 
 
 
